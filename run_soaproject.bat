c3d9cc0149620a47ac079d42f963b3ed118d8393
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2020-07-03 01:39:11 +0530

    run_soaproject: compile all files into one

--- run_soaproject.bat
+++ run_soaproject.bat
@@ -7,20 +7,68 @@ if %errorlevel% equ 1 goto yes
 if %errorlevel% equ 2 goto no
 
 :yes
-echo - Compiling jdatabase
-timeout 2 /nobreak > nul
-
 cd mybankdatabase
-start "Compile jdatabase common object" cmd.exe /c "run_jdatabase"
-cd ..
-timeout 15 /nobreak > nul
+echo - Compiling jdatabase package
+echo:
+javac -cp "json-simple-1.1.1.jar";"mysql-connector-java-8.0.18.jar"; jdatabase\*.java > run_jdatabase.log 2>&1
+echo - Done
 
 echo:
-echo - Compiling jevent
-cd mybankeventclient
-start "Compile jeventClient common object" cmd.exe /c "run_jevent"
+echo - Making jdatabase.jar
+echo:
+jar cvf jdatabase.jar ./jdatabase/*.class json-simple-1.1.1.jar mysql-connector-java-8.0.18.jar >> run_jdatabase.log 2>&1
+echo - Done
+
+echo:
+echo - Installing mysql-connector-java-8.0.18.jar
+echo:
+call mvn install:install-file -Dfile=%working_dir%\mybankdatabase\mysql-connector-java-8.0.18.jar -DgroupId=mysql -DartifactId=mysql-connector-java -Dversion=8.0.18 -Dpackaging=jar -DgeneratePom=true >> run_jdatabase.log 2>&1
+echo - Done
+
+echo:
+echo - Installing json-simple-1.1.1.jar
+echo:
+call mvn install:install-file -Dfile=%working_dir%\mybankdatabase\json-simple-1.1.1.jar -DgroupId=com.googlecode.json-simple -DartifactId=json-simple -Dversion=1.1.1 -Dpackaging=jar -DgeneratePom=true >> run_jdatabase.log 2>&1
+echo - Done
+
+echo:
+echo - Installing jdatabase.jar
+echo:
+call mvn install:install-file -Dfile=%working_dir%\mybankdatabase\jdatabase.jar -DgroupId=mysql -DartifactId=jdatabase -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true >> run_jdatabase.log 2>&1
+echo - Done
+
+echo:
+echo ------------------------------
+echo - Testing jdatabase connection
+echo ------------------------------
+timeout 2 > nul
+echo:
+java -cp "json-simple-1.1.1.jar";"mysql-connector-java-8.0.18.jar"; jdatabase.dbsql
+echo - Done
+echo:
 cd ..
-timeout 20 /nobreak > nul
+
+cd mybankeventclient
+echo - Compiling jeventClient
+echo:
+call mvn clean compile > run_jevent.log 2>&1
+
+echo - Making jevent.jar
+cd .\target\classes
+jar cvf jevent.jar jevent\*.class >> run_jevent.log 2>&1
+cd ..\..
+copy .\target\classes\jevent.jar >> run_jevent.log 2>&1
+echo:
+echo - Installing jevent.jar
+echo:
+call mvn install:install-file -Dfile=%working_dir%\mybankeventclient\jevent.jar -DgroupId=myevent -DartifactId=jevent -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true >> run_jevent.log 2>&1
+
+echo ---------------------------------
+echo - Testing jeventClient connection
+echo ---------------------------------
+call mvn exec:java
+echo:
+cd ..
 goto no
 
 :no
